e32c641f1ac3c53ab1dd488440ec06fbf28a8673
unknown <annahrishko@ukr.net>
2023-04-19 21:08:40 +0300

    change js-file

--- meest_questions/js/script.js
+++ meest_questions/js/script.js
@@ -7,3 +7,13 @@
     return false;
   });
 
+
+$('.item_menu_navigations').click(function(){
+   let index = $(this).attr("menu-tag") 
+   $(".faq_wrap").fadeOut(0)
+   $('.wrap' + index).fadeIn(200);
+    if (index == 1) {
+      $(".faq_wrap").fadeIn(200)
+    }
+   return false
+})
